234f9260c057dc44dd0c2b1f5f0504b5d2298297
Oleg <iforless@yandex.ru>
2019-04-28 14:03:15 +0400

    Коммит 28.04.2019

---
+++ Lesson_09_Uslovia/js/script.js
@@ -0,0 +1,46 @@
+// let x = 5
+// console.log (x++)
+
+// console.log([] + false - null + true)
+
+// let y = 1; 
+// let x = y = 2;
+// console.log(x)
+
+
+//console.log ([ ] + 1 + 2 )
+//console.log ("1"[0])
+//console.log( 2 && 1 && null && 0 && undefined)
+//Есть ли разница между выражениями? !!( a && b ) и (a && b)?
+
+// let a = true
+// let b = true
+
+// if((a && b) === (a && b)){
+// 	console.log('truth')
+// }else{
+// 	console.log('false')
+// }
+
+console.log(!!2)
+
+// let a = [1, 2, 3]; 
+// let b = [1, 2, 3];
+// if(a==b){
+// 	console.log('truth')
+// }else{
+// 	console.log('false')
+// }
+
+//console.log(+"Infinity" )
+
+// if( "Ёжик" > "яблоко"){
+// 	console.log('truth')
+// }else{
+// 	console.log('false')
+// }
+
+//console.log( "" || 2 || undefined || true || falsе)
+
+
+
---
+++ Lesson_09_Uslovia/js/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+let num = 50;
+
+if (num < 49){
+	console.log("Неверно")	
+} else if (num > 100){
+	console.log("Много")
+} else {
+	console.log("Верно");
+}
