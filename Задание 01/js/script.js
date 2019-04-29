e9c808edb7fcb58db4823b477ff2c23c3e9c0c32
Oleg <iforless@yandex.ru>
2019-04-29 14:49:08 +0400

    Задание 3. Приложение часть 1.

--- Задание 01/js/script.js
+++ Задание 01/js/script.js
@@ -1,5 +1,5 @@
 
-let money = prompt ('Ваш бюджет в месяц?', '');
+let money = +prompt ('Ваш бюджет в месяц?', ''); // Плюс чтобы из строки получить число
 let time = prompt ('Введите дату в формате YYYY-MM-DD', '');
 
 let appData = {
@@ -11,14 +11,37 @@ let appData = {
 	savings: false
 };
 
-let a1 = prompt ('Введите обязательную статью расходов в этом месяце', '');
-let a2 = prompt ('Во сколько обойдется?', '');
-let a3 = prompt ('Введите обязательную статью расходов в этом месяце', '');
-let a4 = prompt ('Во сколько обойдется?', '');
+//console.log (appData.budget)
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
+for (let i = 0; i < 2; i++){
+	let a = prompt ('Введите обязательную статью расходов в этом месяце', '');
+	let b = prompt ('Во сколько обойдется?', '');
 
-alert ('Бюджет на один день: ' + appData.budget / 30 );
+	if ( (typeof(a)) === 'string' && (typeof(a)) !== null && (typeof(b)) !== null && a !== "" && b !== "" && a.length < 50){
+		
+		appData.expenses[a] = b; // в качестве ключей передаст значения из переменной "a", если написать через точку, то ключом будет буква "a" 
+		console.log('Done!')
+	} else {
+		console.log('Bad result!')
+		i--
+	}
 
-//console.log(appData.expenses.a3);
+	
+}
+
+appData.moneyPerDay = appData.budget / 30
+//console.log(appData.moneyPerDay);
+
+alert ('Бюджет на один день: ' + appData.moneyPerDay);
+
+console.log(appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100){
+	console.log("Минимальный уровень достатка")
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
+	console.log("Средний уровень достатка")
+} else if (appData.moneyPerDay > 2000){
+	console.log("Высокий уровень достатка")
+} else {
+	console.log("Произошла ошибка")
+}
